51c32431d28590661bb90526257f0ac9962974be
Beren <bmillidgework@gmail.com>
2020-01-24 18:31:07 +0000

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="pmbrl",
+    version="1.0.0",
+    description=(
+        "Planning Model Based RL"
+    ),
+    license="Apache 2.0",
+    url="https://github.com/Bmillidgework/pmbrl",
+    packages=setuptools.find_packages(),
+)
