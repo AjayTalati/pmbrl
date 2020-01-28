00650e480893038ccfb3cfe6ece9da05155432ea
Beren <bmillidgework@gmail.com>
2020-01-28 11:46:26 +0000

    remove env dependence which isn't used anyhow

--- pmbrl/__init__.py
+++ pmbrl/__init__.py
@@ -1,4 +1,4 @@
-from .env import GymEnv, NoisyEnv
+#from .env import GymEnv, NoisyEnv
 from .normalizer import TransitionNormalizer
 from .buffer import Buffer
 from .models import RewardModel, EnsembleModel, EnsembleRewardModel
